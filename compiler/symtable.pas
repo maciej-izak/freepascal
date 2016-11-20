25ed2a4d682392ca4a14854d5573fcc503b59ee1
maciej-izak <hnb.code@gmail.com>
2016-11-20 20:36:25 +0100

    New tokens for new management operators: Initialize, Finalize, Copy and Clone. Small code adjustment for parser (pdecobj.pas, pstatmnt.pas, psub.pas, ptype.pas) was required because few of old token has from now value which is outside range of set: _CONSTRUCTOR, _FINALIZATION.
    New tokens for operators:
      * _OP_INITIALIZE, _OP_FINALIZE, _OP_COPY, _OP_CLONE
    
    New tokens:
      * _INITIALIZE, _FINALIZE, _COPY, _CLONE

--- compiler/symtable.pas
+++ compiler/symtable.pas
@@ -422,6 +422,10 @@ interface
     { _ASSIGNMENT    }  'assign',
     { _OP_EXPLICIT   }  'explicit',
     { _OP_ENUMERATOR }  'enumerator',
+    { _OP_INITIALIZE }  'initialize',
+    { _OP_FINALIZE   }  'finalize',
+    { _OP_COPY       }  'copy',
+    { _OP_CLONE      }  'clone',
     { _OP_INC        }  'inc',
     { _OP_DEC        }  'dec');
 
