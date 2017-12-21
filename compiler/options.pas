51b2cc4162c5a8bbdf0140076453307734de4bbe
maciej-izak <hnb.code@gmail.com>
2017-12-21 15:34:52 +0100

    Test for patch for AVR tests (bug #32809 related to rev. 37708)

--- compiler/options.pas
+++ compiler/options.pas
@@ -4256,10 +4256,9 @@ begin
   if target_info.system in systems_indirect_var_imports then
     def_system_macro('FPC_HAS_INDIRECT_VAR_ACCESS');
 
-  if cs_compilesystem in init_settings.moduleswitches then
-    for i:=low(tfeature) to high(tfeature) do
-      if i in features then
-        def_system_macro('FPC_HAS_FEATURE_'+featurestr[i]);
+  for i:=low(tfeature) to high(tfeature) do
+    if i in features then
+      def_system_macro('FPC_HAS_FEATURE_'+featurestr[i]);
 
 {$push}
 {$warn 6018 off} { Unreachable code due to compile time evaluation }
